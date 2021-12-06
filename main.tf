765dd9eb4e1acc87c0d9d1ca86d145dbe0a1ef1e
Markus Lu <markuslu.bc@gmail.com>
2021-12-06 08:05:22 -0800

    Finished VPC

--- main.tf
+++ main.tf
@@ -12,3 +12,25 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+module "custom_vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr = var.vpc_cidr
+
+  lb_pub_sub_1 = "172.31.1.0/24"
+  lb_pub_sub_2 = "172.31.2.0/24"
+  lb_pub_sub_3 = "172.31.3.0/24"
+
+  lb_pri_sub_1 = "172.31.4.0/24"
+  lb_pri_sub_2 = "172.31.5.0/24"
+  lb_pri_sub_3 = "172.31.6.0/24"
+
+  app_pri_sub_1 = "172.31.7.0/24"
+  app_pri_sub_2 = "172.31.8.0/24"
+  app_pri_sub_3 = "172.31.9.0/24"
+
+  az_1 = "${var.region}a"
+  az_2 = "${var.region}b"
+  az_3 = "${var.region}c"
+}
